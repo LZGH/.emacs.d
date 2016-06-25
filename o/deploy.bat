e09146c8c34b4d432214ef22dfcc097af925ed35
LZGH <1183071782@qq.com>
2016-06-25 21:36:05 +0800

    emacs 123

--- o/deploy.bat
+++
@@ -1,7 +0,0 @@
-F:
-cd apache-tomcat-7.0.68
-call F:\apache-tomcat-7.0.68\bin\shutdown.bat
-rd /s/q F:\apache-tomcat-7.0.68\work\Catalina\localhost
-del/s/q F:\apache-tomcat-7.0.68\webapps\
-move "C:\Users\lz\Desktop\123.bat" "F:\apache-tomcat-7.0.68\webapps"
-call F:\apache-tomcat-7.0.68\bin\startup.bat
