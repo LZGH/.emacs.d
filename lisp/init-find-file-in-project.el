616ab499c5c85b06e8e9f29955b7751e85918996
LZGH <1183071782@qq.com>
2016-07-08 08:42:00 +0800

    emacs

--- lisp/init-find-file-in-project.el
+++ lisp/init-find-file-in-project.el
@@ -9,20 +9,18 @@
 (autoload 'find-file-in-project "find-file-in-project-by-selected" nil t)
 (autoload 'find-file-in-project "find-directory-in-project-by-selected" nil t)
 
+(setq ffip-project-root "/")
 ;; well, I'm not interested in concatenated BIG js file or file in dist/
 (setq ffip-find-options "-not -size +64k -not -iwholename '*/dist/*'")
 ;; for this project, I'm only interested certain types of files
-(setq ffip-patterns '("*.html" "*.js" "*.css" "*.java" "*.xml" "*.el"))
+;;(setq ffip-patterns '("*.html" "*.js" "*.css" "*.java" "*.xml" "*.el"))
 ;; exclude below directories and files
-(setq ffip-prune-patterns '("*/.metadata/*" "*/classes/*" "*/tourism-cms/*" "*/tourism-parent/*"  "*/test/*"
-"*/tourism-web/*" "*/CarRescue/*" "*/Forum_Server/*" "*/Ship_PC/*" "*/Ship_Server/*" "*/Ship_Test/*" "*/CarFormat_Server/*"
-"*/Ship_WeChat/*" "*/wmy_cms/*" "*/Wmy_Server/*" "*/push_demo/*" "*/wmy_cms_content/*"
-"*/Wmy_web/*" "*/WXXC/*"))
+(setq ffip-prune-patterns '("*/.metadata/*" "*/classes/*"))
 
 (if (eq system-type 'windows-nt)
     ;; Native Windows
     (setq ffip-project-root "D:/eclipse_workspace")
-)
+  )
 
 (global-set-key (kbd "C-x f") 'find-file-in-project)
 (global-set-key (kbd "C-c f") 'find-file-in-project-by-selected)
