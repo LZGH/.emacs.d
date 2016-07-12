0ba4fbfef1c39e3bf9df2a4a8fbee72b8a7f294e
LZGH <1183071782@qq.com>
2016-07-12 18:09:10 +0800

    emacs

---
+++ custom.el
@@ -0,0 +1,29 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(column-number-mode t)
+ '(cua-mode t nil (cua-base))
+ '(custom-enabled-themes (quote (sanityinc-solarized-dark)))
+ '(custom-safe-themes (quote ("4aee8551b53a43a883cb0b7f3255d6859d766b6c5e14bcb01bed572fcbef4328" default)))
+ '(safe-local-variable-values (quote ((no-byte-compile t))))
+ '(save-place t nil (saveplace))
+ '(session-use-package t nil (session))
+ '(show-paren-mode t)
+ '(speedbar-hide-button-brackets-flag t)
+ '(speedbar-show-unknown-files t)
+ '(speedbar-smart-directory-expand-flag t)
+ '(speedbar-use-images nil)
+ '(sr-speedbar-auto-refresh nil)
+ '(sr-speedbar-max-width 50)
+ '(sr-speedbar-right-side nil)
+ '(sr-speedbar-skip-other-window-p t)
+ '(sr-speedbar-width 40 t)
+ '(tool-bar-mode nil))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(default ((t (:family "Courier New" :foundry "outline" :slant normal :weight normal :height 143 :width normal)))))
---
+++ org-clock-save.el
@@ -0,0 +1,1 @@
+;; org-persist.el - DESKTOP-3H62FIJ at <2016-07-12 周二 17:31>
